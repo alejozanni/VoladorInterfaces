da177df869a46275eab42719c3bec2f818bd14b9
Alejo Zanni <56770851+alejozanni@users.noreply.github.com>
2021-10-15 12:07:53 -0300

    Add files via upload

---
+++ Volador/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Volador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pato pato = new Pato();
+            Boing747 boing747 = new Boing747();
+            Superman superman = new Superman();
+            TorreDeControl torreDeControl = new TorreDeControl();
+
+            torreDeControl.agregarVolador(superman);
+            torreDeControl.agregarVolador(boing747);
+            torreDeControl.agregarVolador(pato);
+
+            torreDeControl.vuelenTodos();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Volador/TorreDeControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volador
+{
+    class TorreDeControl
+    {
+        List<Ivolador> voladores = new List<Ivolador>();
+
+        public void vuelenTodos()
+        {
+            foreach(Ivolador v in voladores)
+            {
+                v.volador();
+            }
+        }
+
+        public void agregarVolador(Ivolador unVolable)
+        {
+            voladores.Add(unVolable);
+        }
+    }
+}
